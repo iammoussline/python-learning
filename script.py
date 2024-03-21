75bc55cbc51a643086d88268e57c8e9a5b80cfd9
iammoussline <mouhammed.kandemir@gmail.com>
2024-03-21 01:54:00 +0100

    First

--- script.py
+++ script.py
@@ -1,5 +1,11 @@
 #coding:utf-8
 
-print("Hello World !")
+# Voici un commentaire sur une ligne
+
+""" Commentaire sur 
+plusieurs lignes """
+
+print("Hello World !\nHello World !")
+# '\n' : Saut de ligne - '\t' : Tabulation
 print("Hello World !")
 
