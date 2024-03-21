4ee9a99d49802197129907d941ad49c5e8c59469
iammoussline <mouhammed.kandemir@gmail.com>
2024-03-21 22:27:23 +0100

    Python 3

---
+++ boucle.py
@@ -0,0 +1,20 @@
+#coding:utf-8
+"""
+i = 0
+
+while i < 10:
+    print("Je suis une phrase")
+    i += 1
+"""
+
+"""
+Mots-clés : break (casse la boucle)
+            continue (revient au début de la boucle)
+"""
+
+
+
+phrase ="Hello WWorld !"
+
+for letter in phrase:
+    print(letter)
