ee2b3f9f8995a12a53d7dc5ecbc93d2ffb7445d6
iammoussline <mouhammed.kandemir@gmail.com>
2024-03-21 23:34:23 +0100

    Python 3

---
+++ fonctions.py
@@ -0,0 +1,60 @@
+#coding:utf-8
+"""
+FONCTION DIRE BONJOUR
+def dire_bonjour():
+    print("Bonjour !")
+
+dire_bonjour()
+"""
+
+
+
+
+
+
+"""
+def dire(nom_personne, message_personne, age):
+    print("{} : {}".format(nom_personne, message_personne))
+
+dire("Mouss", "Salut beau-goss !", 18)
+dire("Joe", "Salut !", 23)
+"""
+
+
+
+
+"""
+def show_inventory(*list_items): #l'étoile veut dire qu'il y a un nombre inconnu de paramètre
+    for item in list_items:
+        print(item)
+
+show_inventory("épée")
+show_inventory("épée", "arc", "armure", "oiseau")
+"""
+
+
+"""
+def calculer_somme(nombre1, nombre2):
+    return nombre1 + nombre2
+
+
+print(calculer_somme(4, 6))
+"""
+
+
+
+"""
+def comparer_nombre(nombre1, nombre2):
+    if nombre1 > nombre2:
+        return nombre1
+    elif nombre1 < nombre2:
+        return nombre2
+    else:
+        return "Egalité !"
+    
+    
+print(comparer_nombre(15, 15))
+"""
+
+
+
