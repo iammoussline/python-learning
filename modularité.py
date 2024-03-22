19d6e403d07ee8bcc400db08bddd7384660aba22
iammoussline <mouhammed.kandemir@gmail.com>
2024-03-22 22:03:08 +0100

    Python4

---
+++ modularité.py
@@ -0,0 +1,67 @@
+#codint:utf-8
+
+"""
+Important un module :   import <nom module>
+                        from <nom module> import <nom fonction>
+                        from <nom module> import *
+"""
+"""
+import math
+
+
+resultat = math.sqrt(100)
+print(resultat)
+
+
+sinus = math.sin(42)
+print(sinus)
+"""
+
+
+
+
+"""
+import os
+
+os.system("clear")
+"""
+
+
+
+
+"""
+import modularitétest
+
+modularitétest.au_revoir()
+modularitétest.parler("Mouss", "Bienvenue !")
+"""
+
+
+
+import modularitétest
+
+modularitétest.au_revoir()
+modularitétest.parler("Mouss", "Bienvenue !")
+
+
+
+"""
+import dossier.fichier (as player pour raccourccir l'appelle de la fonction)
+
+
+si le fichier a appelé est dans un dossier
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
--- mudularité.py
+++
@@ -1,2 +0,0 @@
-#codint:utf-8
-
