3ada9fa3c7a06876e471ff22a58e9c2f021e5616
iammoussline <mouhammed.kandemir@gmail.com>
2024-04-23 13:24:45 +0200

    Mise à jour script2.py -> recherche_de_fichiers.py

---
+++ recherche_de_fichiers.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+def rechercher_fichiers(repertoire, extension):
+    """Recherche des fichiers dans un répertoire et ses sous-répertoires."""
+    fichiers_trouves = []
+    for dossier, sous_dossiers, fichiers in os.walk(repertoire):
+        for fichier in fichiers:
+            if fichier.endswith(extension):
+                chemin_fichier = os.path.join(dossier, fichier)
+                fichiers_trouves.append(chemin_fichier)
+    return fichiers_trouves
+
+# Vérifier si l'utilisateur a fourni l'extension du fichier en argument
+if len(sys.argv) < 3:
+    print("Usage: python recherche_fichiers.py <répertoire> <extension>")
+    sys.exit(1)
+
+repertoire = sys.argv[1]
+extension = sys.argv[2]
+
+fichiers_trouves = rechercher_fichiers(repertoire, extension)
+
+if fichiers_trouves:
+    print("Fichiers trouvés :")
+    for fichier in fichiers_trouves:
+        print(fichier)
+else:
+    print("Aucun fichier correspondant à l'extension spécifiée n'a été trouvé.")
+
